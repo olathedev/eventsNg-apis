d428808b5ea0857059cf793535a0f729597cc418
DevOlaMicheal <olapademicheal3@gmail.com>
2024-03-05 16:54:53 +0100

    bugFix

--- validators/event-validator.js
+++ validators/event-validator.js
@@ -3,7 +3,7 @@ import Joi from "joi";
 const Schema = Joi.object({
 
     title: Joi.string().required().min(2).max(100),
-    description: Joi.string().required().min(2).max(100),
+    description: Joi.string().required().min(2).max(300),
     location: Joi.string().required().min(2).max(100),
     time: Joi.required(),
     eventDate: Joi.required(),
